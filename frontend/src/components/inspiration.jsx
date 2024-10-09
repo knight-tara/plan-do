288c1b59753b7972fe6902f1603677156934aca9
Tara <taconlin@hotmail.co.uk>
2024-10-09 18:41:10 +0100

    fixed loading position on inspiration component

--- frontend/src/components/inspiration.jsx
+++ frontend/src/components/inspiration.jsx
@@ -3,9 +3,6 @@ import { ComponentContainer } from "./componentContainer";
 import { InspirationCard } from "./inspirationCard"
 
 export const Inspiration = ({ loading, inspirationResults }) => {
-    if (loading) {
-        return <p>Loading ...</p>;
-    }
 
     if (!inspirationResults || inspirationResults.length === 0) {
         return <p>No results found</p>;
@@ -15,7 +12,8 @@ export const Inspiration = ({ loading, inspirationResults }) => {
         <ComponentContainer>
         <div>
             <h2>Plan-Do</h2>
-            <h1>Here are your results ...</h1>
+            {loading ? 
+            <h1>Loading ... </h1> : <h1>Here are your results ...</h1>}
             <InspirationCard loading={loading} inspiration={inspirationResults[0]}/>
             <InspirationCard loading={loading} inspiration={inspirationResults[1]}/>
             <InspirationCard loading={loading} inspiration={inspirationResults[2]}/>
